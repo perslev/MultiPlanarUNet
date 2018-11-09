3ceda0845bb0aa5ef8e706ae9eab94bf34e888cd
Mathias Perslev <mathias@perslev.com>
2018-11-09 10:26:00 +0100

    model_init function now fetches the learning rate used at the epoch from which training is continued when --continue flag is set; model_init function now cleans up the logs/training.csv file in continued training mode (removes entires from old runs etc)

--- models/model_init.py
+++ models/model_init.py
@@ -19,14 +19,24 @@ def model_initializer(hparams, continue_training, base_path, logger=None):
     model = init_model(hparams["build"], logger)
 
     if continue_training:
-        from MultiViewUNet.utils import get_last_model
+        from MultiViewUNet.utils import get_last_model, get_lr_at_epoch, \
+                                        clear_csv_after_epoch
         model_path, epoch = get_last_model(os.path.join(base_path, "model"))
         model.load_weights(model_path, by_name=True)
         hparams["fit"]["init_epoch"] = epoch+1
 
+        # Get the LR at the continued epoch
+        lr, name = get_lr_at_epoch(epoch, os.path.join(base_path, "logs"))
+        hparams["fit"]["optimizer_kwargs"][name] = lr
+
+        # Remove entries in training.csv file that occurred after the
+        # continued epoch
+        clear_csv_after_epoch(epoch, os.path.join(base_path, "logs", "training.csv"))
+
         logger("[NOTICE] Training continues from:\n"
                "Model: %s\n"
-               "Epoch: %i\n" % (os.path.split(model_path)[-1], epoch))
+               "Epoch: %i\n"
+               "LR:    %s" % (os.path.split(model_path)[-1], epoch, lr))
     else:
         hparams["fit"]["init_epoch"] = 0
 
