7064c3d227cbe96581cc700c0b608187ebecdb8c
Mathias Perslev <mathias@perslev.com>
2018-11-09 14:54:40 +0100

    Removed depricated import

--- sequences/__init__.py
+++ sequences/__init__.py
@@ -2,4 +2,3 @@ from .patch_sequence_3d import PatchSequence3D
 from .sliding_patch_sequence_3d import SlidingPatchSequence3D
 from .isotrophic_live_view_sequence_2d import IsotrophicLiveViewSequence2D
 from .isotrophic_live_view_sequence_3d import IsotrophicLiveViewSequence3D
-from .point_sequence import PointSequence
