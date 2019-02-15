d76b1a819b7d06d85a3dc74a49ff8ab55a4c81bd
Mathias Perslev <mathias@perslev.com>
2019-02-15 15:10:32 +0100

    Added utility function which checks a if the parameters in a dictionary are all contained in a list (sometimes used to check if inputs are valid)

--- MultiPlanarUNet/utils/__init__.py
+++ MultiPlanarUNet/utils/__init__.py
@@ -3,4 +3,5 @@ from .utils import get_free_gpu, get_best_model, get_last_model, \
                    create_folders, get_class_weights, print_options_context, \
                    pred_to_class, await_and_set_free_gpu, set_gpu, add_noise_to_views, \
                    DummyContext, highlighted, get_free_gpus, wait_for, \
-                   get_lr_at_epoch, clear_csv_after_epoch, random_split
+                   get_lr_at_epoch, clear_csv_after_epoch, random_split,\
+                   check_kwargs
