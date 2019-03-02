4c852f9f4cd8d3b9754c7687d1e92de85ace0b9b
Mathias Perslev <mathias@perslev.com>
2019-03-02 19:26:46 +0100

    Added functionality to set revert/fast-forward to a specific commit version by commit ID

--- MultiPlanarUNet/bin/version/version_controller.py
+++ MultiPlanarUNet/bin/version/version_controller.py
@@ -40,8 +40,8 @@ class VersionController(object):
     def branch(self):
         return self.git_query("git symbolic-ref --short HEAD")
 
-    def set_commit(self):
-        pass
+    def set_commit(self, commit_id):
+        self.git_query("git reset --hard {}".format(str(commit_id)[:7]))
 
     def set_branch(self, branch):
         self.git_query("git checkout {}".format(branch))
@@ -49,5 +49,4 @@ class VersionController(object):
     def set_version(self, version):
         version = str(version).lower().strip(" v")
         self.set_branch("v{}".format(version))
-
-        pass
+        self.set_commit(self.get_latest_commit_in_branch())
