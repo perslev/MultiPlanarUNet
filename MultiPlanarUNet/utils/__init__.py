467936a4120e5720f938a8c7f5e4124a7a2e94da
Mathias Perslev <mathias@perslev.com>
2019-03-12 15:15:15 +0100

    Added get_last_epoch function

--- MultiPlanarUNet/utils/__init__.py
+++ MultiPlanarUNet/utils/__init__.py
@@ -5,4 +5,4 @@ from .utils import get_free_gpu, get_best_model, get_last_model, \
                    DummyContext, highlighted, get_free_gpus, wait_for, \
                    get_lr_at_epoch, clear_csv_after_epoch, random_split,\
                    check_kwargs, arr_to_fixed_precision_string, ensure_list_or_tuple, \
-                   await_PIDs
+                   await_PIDs, get_last_epoch
