46f93296a41a68a3ec7ca30add2d02a5adc716c6
Mathias Perslev <mathias@perslev.com>
2019-09-25 09:47:53 +0200

    Added (early version) of toy data generator script

--- MultiPlanarUNet/bin/toy_data.py
+++ MultiPlanarUNet/bin/toy_data.py
@@ -2,9 +2,9 @@ import os
 from argparse import ArgumentParser
 import readline
 import numpy as np
-import pandas as pd
 import nibabel as nib
 
+from scipy.ndimage.filters import gaussian_filter
 from MultiPlanarUNet.utils.utils import create_folders
 
 readline.parse_and_bind('tab: complete')
@@ -40,9 +40,117 @@ def get_argparser():
     return parser
 
 
+def _elastic_deform(xx, yy, zz, sigma, alpha):
+    dx = gaussian_filter((np.random.rand(*xx.shape) * 2 - 1), sigma,
+                         mode="constant", cval=0.) * alpha
+    dy = gaussian_filter((np.random.rand(*yy.shape) * 2 - 1), sigma,
+                         mode="constant", cval=0.) * alpha
+    dz = gaussian_filter((np.random.rand(*zz.shape) * 2 - 1), sigma,
+                         mode="constant", cval=0.) * alpha
+    return xx+dx, yy+dy, zz+dz
+
+
+def _get_center(img_size):
+    min_, max_ = img_size * 0.05, img_size * 0.95
+    range_ = max_ - min_
+    center = np.random.rand(3) * range_ - min_
+    return center.astype(np.int)
+
+
+def create_toy_image(img_size):
+    # Create empty image
+    im = np.zeros(shape=[img_size]*3, dtype=np.float64)
+
+    # Create grid
+    xx, yy, zz = np.mgrid[:img_size, :img_size, :img_size]
+    xx_d, yy_d, zz_d = _elastic_deform(xx, yy, zz,
+                                       sigma=img_size/6,
+                                       alpha=img_size*60)
+
+    # Add background
+    im += np.sin(0.05 + np.random.rand() * xx/img_size) + \
+          np.power(np.cos(0.05 + np.random.rand() * yy/img_size), 0.5) + \
+          np.power(np.sin(0.05 + np.random.rand() * zz/img_size), 2)
+
+    # Scale 0-0.75
+    im -= im.min()
+    im /= im.max() * (1/0.75)
+
+    # Add sphere
+    radius = img_size/8 + np.random.rand()*img_size/2
+    center = _get_center(img_size)
+    sphere_mask = np.power(xx_d-center[0], 2) + \
+                  np.power(yy_d-center[1], 2) + \
+                  np.power(zz_d-center[2], 2) <= np.power(radius, 2)
+    sphere_vals = np.sin((xx[sphere_mask]-center[0])/img_size) + \
+                  np.sin((yy[sphere_mask]-center[1])/img_size*5) + \
+                  np.sin((zz[sphere_mask]-center[2])/img_size*10)
+
+    # Add and scale 0.2-1.0
+    im[sphere_mask] += sphere_vals
+    im[sphere_mask] -= im[sphere_mask].min()
+    im[sphere_mask] /= im[sphere_mask].max() * 1/0.8
+    im[sphere_mask] += 0.2
+
+    # Add elastic square
+    size = img_size/4 + np.random.rand()*img_size/2
+    center = _get_center(img_size)
+    square_mask = (np.abs(xx_d-center[0]) < size/2) & \
+                  (np.abs(yy_d-center[1]) < size/2) & \
+                  (np.abs(zz_d-center[2]) < size/2)
+    square_vals = np.power(xx[square_mask]-center[0], 2) + \
+                  np.power(yy[square_mask]-center[1], 2) + \
+                  np.power(zz[square_mask]-center[2], 2)
+
+    # Add and scale 0.2-1.0
+    im[square_mask] += square_vals
+    im[square_mask] += square_vals
+    im[square_mask] -= im[square_mask].min()
+    im[square_mask] /= im[square_mask].max() * 1/0.8
+    im[square_mask] += 0.2
+
+    # Add torus
+    center_radius = img_size/16 + np.random.rand()*img_size/6
+    tube_radius = img_size/32 + np.random.rand()*img_size/12
+    center = _get_center(img_size)
+    torus_mask_1 = np.power(center_radius-np.sqrt(np.power(xx_d-center[0], 2) +
+                                                  np.power(yy_d-center[1], 2)), 2) + \
+                   np.power(zz_d-center[2], 2) <= np.power(tube_radius, 2)
+    torus_mask_2 = np.power(center_radius-np.sqrt(np.power(zz_d-center[2], 2) +
+                                                  np.power(yy_d-center[1], 2)), 2) + \
+                   np.power(xx_d-center[0], 2) <= np.power(tube_radius, 2)
+    torus_mask = np.logical_or(torus_mask_1, torus_mask_2)
+    torus_vals = np.random.poisson(im[torus_mask])
+
+    # Add and scale 0.2-1.0
+    im[torus_mask] += torus_vals
+    im[torus_mask] -= im[torus_mask].min()
+    im[torus_mask] /= im[torus_mask].max() * 1 / 0.8
+    im[torus_mask] += 0.2
+
+    # Add noise to image
+    im += np.random.poisson(lam=10, size=im.shape) * np.random.randn(*im.shape) * 0.0005
+
+    # Scale overall image to [0, ..., 1]
+    im -= im.min()
+    im /= im.max()
+
+    # Scale to 16 bit int
+    im *= np.iinfo(np.uint16).max
+    return im.astype(np.uint16), sphere_mask, square_mask, torus_mask
+
+
+def create_toy_labels(*masks):
+    assert len(masks) < np.iinfo(np.uint8).max
+    labels = np.zeros(shape=masks[0].shape, dtype=np.uint8)
+    for i, mask in enumerate(masks):
+        labels[mask] = i+1
+    return labels
+
+
 def create_toy_data_point(img_size):
-    im = np.random.randn(*[img_size]*3).astype(np.float32)
-    lab = np.random.randint(0, 5, size=[img_size*3]).astype(np.uint8)
+    im, m1, m2, m3 = create_toy_image(img_size)
+    lab = create_toy_labels(m1, m2, m3)
     return im, lab
 
 
@@ -52,6 +160,7 @@ def create_toy_dataset(N, image_size, out_dir, image_subdir, label_subdir):
     create_folders([out_dir, image_out_dir, label_out_dir])
 
     for img_id in range(N):
+        print("--", img_id)
         file_name = "toy_data_{}.nii.gz".format(img_id)
         im_path = os.path.join(image_out_dir, file_name)
         lab_path = os.path.join(label_out_dir, file_name)
